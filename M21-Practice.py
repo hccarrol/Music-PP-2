5cf9f6600761f80334557de87c5a481350ef2e1b
Holden Carroll <hccarrol@iu.edu>
2026-02-03 15:00:21 +0100

    Showing Streams and Notes
    Creating and showing streams and notes on musescore 4

--- M21-Practice.py
+++ M21-Practice.py
@@ -1,4 +1,22 @@
-from music21 import note, stream
+from music21 import note, stream, environment
+
+
+us = environment.UserSettings()
+us['musicxmlPath'] = '//Applications/MuseScore 4.app'
+us['musescoreDirectPNGPath'] = '/Applications/MuseScore 4.app'
+
+s = stream.Stream()
+s.append(note.Note('C4', quarterLength=1))
+s.append(note.Note('E4', quarterLength=1))
+s.append(note.Note('G4', quarterLength=1))
+
+# Just write to MIDI - this doesn't need MuseScore at all
+s.write('midi', fp='test.mid')
+print("MIDI file created successfully!")
+
+
+
 
 n1 = note.Note('C4')
-n1.show()
+#n1.show()
+s.show()
